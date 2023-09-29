af25982f50e724273f1dd239cfea603aed9ef700
tk <62570383+tksoftw@users.noreply.github.com>
2023-09-29 04:05:38 -0700

    updates

--- script.js
+++ script.js
@@ -1,28 +1,63 @@
-window.addEventListener('mousemove', event => {
-    updateCounterContent(event.clientX, event.clientY)
-});
+let xVelocityDVD = 1;
+let yVelocityDVD = 1;
 
-function updateCounterContent(x, y) {
-    value = document.getElementById('counter');
-    value.innerHTML = `${x}, ${y}`;
-    moveTo(value, x, y);
-    if (isOnCorner(value)) {
-        alert('You are on a corner!');
-        value.innerHTML = 'CORNER';
+let isButtonPressed = false;
+
+document.addEventListener('mousedown', () => !isButtonPressed && ((isButtonPressed = true) || loop()));
+document.addEventListener('mouseup', () => isButtonPressed = false);
+
+window.onload = moveDVDLoop;
+
+
+function animate(func, fps) {
+    // perform some animation task here
+    func();
+    setTimeout(() => {
+        requestAnimationFrame(animate);
+    }, 1000 / fps);
+}
+
+function moveDVDLoop() {
+    while (true) {
+        animate(moveDVD, 60);
     }
 }
 
+function moveDVD() {
+    let dvd = document.getElementById('dvd');
+    if (isOnCorner(dvd)) {
+        xVelocityDVD *= -1;
+        yVelocityDVD *= -1;
+    }
+    else if (isOnEdge(dvd)) {
+        let v = getEdgeVelocityVector(dvd);
+        xVelocityDVD *= v[0];
+        yVelocityDVD *= v[1];
+    }
+    pushBy(dvd, xVelocityDVD, -yVelocityDVD);
+}
+
 function moveTo(elem, x, y) {
-    elem.style = `
-    position: absolute;
-    top: ${y}px;
-    left: ${x}px;
-    transform: translate(-50%, -50%);
-    `
+    elem.style.left = `${x}px`;
+    elem.style.top = `${y}px`;
+}
+
+function updateDisplay(x, y) {
+    element = document.getElementById('display');
+    element.innerHTML = `(${x}, ${y})`;
+}
+
+function pushBy(elem, deltaX, deltaY) {
+    style = window.getComputedStyle(elem);
+    // Assuming elem.style.left and elem.style.top are already set
+    let x = parseInt(style.left) + deltaX;
+    let y = parseInt(style.top) + deltaY;
+    updateDisplay(x, isButtonPressed);
+    moveTo(elem, x, y);
 }
 
 function isOnEdge(elem) {
-    var bounding = elem.getBoundingClientRect();
+    let bounding = elem.getBoundingClientRect();
     return (bounding.top < 0 ||
             bounding.left < 0 || 
             bounding.bottom > window.innerHeight ||
@@ -30,9 +65,20 @@ function isOnEdge(elem) {
 }
 
 function isOnCorner(elem) {
-    var bounding = elem.getBoundingClientRect();
+    let bounding = elem.getBoundingClientRect();
     return (bounding.top < 0 && bounding.left < 0) ||
             (bounding.top < 0 && bounding.right > window.innerWidth) ||
             (bounding.bottom > window.innerHeight && bounding.left < 0) ||
             (bounding.bottom > window.innerHeight && bounding.right > window.innerWidth);
+}
+
+function getEdgeVelocityVector(elem) {
+    // assuming elem is on edge
+    let bounding = elem.getBoundingClientRect();
+    if (bounding.left < 0 || bounding.right > window.innerWidth) {
+        return [-1, 1];
+    }
+    else if (bounding.top < 0 || bounding.bottom > window.innerHeight) {
+        return [1, -1];
+    }
 }
